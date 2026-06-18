17dcec94b7ed47077aaf26906246db73bd51dff8
Mela Heß <melanie.hess.74@dci-student.org>
2026-06-18 11:56:51 +0200

    style: all styles in css and added a keyframes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,16 +5,16 @@ export default function Header() {
   const { user, logout } = useAuth();
 
   return (
-    <header style={styles.header}>
-      <h1 style={styles.logo}>GutenRead</h1>
+    <header className="header">
+      <h1 className="header-logo">GutenRead</h1>
 
-      <nav style={styles.nav}>
+      <nav className="header-nav">
         {!user && (
           <>
-            <Link to="/login" style={styles.link}>
+            <Link to="/login" className="header-link">
               Login
             </Link>
-            <Link to="/register" style={styles.link}>
+            <Link to="/register" className="header-link">
               Register
             </Link>
           </>
@@ -22,8 +22,8 @@ export default function Header() {
 
         {user && (
           <>
-            <span style={styles.user}>Hallo, {user.username}</span>
-            <button onClick={logout} style={styles.button}>
+            <span className="header-user">Hallo, {user.username}</span>
+            <button onClick={logout} className="header-button">
               Logout
             </button>
           </>
@@ -32,32 +32,3 @@ export default function Header() {
     </header>
   );
 }
-
-const styles = {
-  header: {
-    padding: "1rem",
-    background: "#2c09f1",
-    color: "white",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  nav: {
-    display: "flex",
-    gap: "1rem",
-  },
-  link: {
-    color: "white",
-    textDecoration: "none",
-  },
-  button: {
-    background: "crimson",
-    color: "white",
-    border: "none",
-    padding: "0.5rem 1rem",
-    cursor: "pointer",
-  },
-  user: {
-    marginRight: "1rem",
-  },
-};
